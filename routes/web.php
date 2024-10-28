8947d6ce5778b9ba326baa5e072195a52a2c20cb
GiorgioDordo <djole6789@gmail.com>
2024-10-28 16:13:23 +0100

    fixed home and train controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class, 'home'])->name('home');
+Route::get('/', [TrainController::class, 'index'])->name('train');
